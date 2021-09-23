93e8d8afca47f3984ef46cf26991fc4e74db8791
Leordrgs <leonardo.rodrigues@catolicasc.org.br>
2021-09-23 20:24:41 -0300

    create cards and adding colors

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,16 +9,65 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        height: 200.0,
-        width: 170.0,
-        decoration: BoxDecoration(
-            color: Color(0xFF1D1F33),
-            borderRadius: BorderRadius.circular(10.0)),
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                color: Color(0xFF1D1F33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+
+  ReusableCard({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
